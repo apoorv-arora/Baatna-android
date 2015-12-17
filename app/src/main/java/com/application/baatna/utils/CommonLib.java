80bec89515ab006222a9cb97f87c727c05a35736
baatna <android@baatna.com>
2015-12-17 17:40:41 +0530

    changing ip

--- app/src/main/java/com/application/baatna/utils/CommonLib.java
+++ app/src/main/java/com/application/baatna/utils/CommonLib.java
@@ -64,8 +64,8 @@ public class CommonLib {
 	public static final boolean isTestBuild = false;
 
 	public static final String SERVER_PREFIX = "http://";
-	public static final String SERVER_BODY = "52.76.14.6:8080/BaatnaServer/rest/";
-	public static String SERVER_WITHOUT_VERSION = "http://52.76.14.6:8080/BaatnaServer/rest/";
+	public static final String SERVER_BODY = "192.168.2.50:8080/BaatnaServer/rest/";
+	public static String SERVER_WITHOUT_VERSION = "http://192.168.2.50:8080/BaatnaServer/rest/";
 	//public static final String SERVER_BODY = "192.168.2.50:8080/BaatnaServer/rest/";
 	//public static String SERVER_WITHOUT_VERSION = "http://192.168.2.50:8080/BaatnaServer/rest/";
 
